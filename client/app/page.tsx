c934a2de552e9e28e2cbfc963594746db6f26452
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2023-03-23 08:52:32 +0100

    add socket.io

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,20 +1,67 @@
 "use client"
 
-import {FC, useState} from 'react'
+import {FC, useEffect, useState} from 'react'
 import { useDraw } from '@/hooks/useDraw'
 import {ChromePicker} from 'react-color'
+import {io, Socket} from 'socket.io-client'
+import { drawLine } from '@/utils/drawLine'
+const socket = io ('http://localhost:5000')
 
 
 interface PageProps {}
 
-const Page: FC <PageProps> = ({}) => {
+type DrawLineProps = {
+  prevPoint: Point | null,
+  currentPoint : Point,
+  color: string
+}
 
-  const {canvasRef , onMouseDown , clear} = useDraw(drowLine)
+
+const Page: FC <PageProps> = ({}) => {
   
   const [color , setColor] = useState<string>('#000')
+  const {canvasRef , onMouseDown , clear} = useDraw(createLine)
 
-  function drowLine ({prevPoint,currentPoint,ctx}: Draw) {
-    const {x:currX , y : currY} = currentPoint
+  useEffect(()=>{
+     
+    const ctx = canvasRef.current?.getContext('2d')
+    
+    socket.emit('client-ready')
+
+    socket.on('get-canvas-state',()=>{
+      if(!canvasRef.current?.toDataURL()) return
+      socket.emit('canvas-state',canvasRef.current?.toDataURL())
+    })
+
+    socket.on('canvas-state-from-server',(state : string)=>{
+      console.log('i recive the state')
+      const img = new Image()
+      img.src = state
+      img.onload = () => {
+        ctx?.drawImage(img,0,0)
+      }
+    })
+
+    socket.on('draw-line',({prevPoint,currentPoint,color}:DrawLineProps) => {
+      if(!ctx) return
+      drawLine({prevPoint,currentPoint,ctx,color})
+    })
+
+    socket.on('clear',clear)
+    
+    return () => {
+      socket.off('get-canvas-state')
+      socket.off('canvas-state-from-server')
+      socket.off('draw-line')
+      socket.off('clear')
+    }
+    
+  },[canvasRef])
+  
+  
+
+  /* function drowLine ({prevPoint,currentPoint,ctx}: Draw) {} */
+    /* const {x:currX , y : currY} = currentPoint
     const lineColor = color
     const lineWidth = 5
 
@@ -29,15 +76,25 @@ const Page: FC <PageProps> = ({}) => {
     ctx.fillStyle = lineColor
     ctx.beginPath()
     ctx.arc(startPoint.x , startPoint.y,2,0,2 * Math.PI)
-    ctx.fill()
-  }
+    ctx.fill() */
+  
 
+  function createLine({prevPoint,currentPoint,ctx}: Draw){
+    socket.emit('draw-line',({prevPoint,currentPoint,color}))
+    drawLine({prevPoint,currentPoint,ctx,color})
+  }
 
   return (
     <div className='w-screen h-screen bg-white flex justify-center items-center'>
       <div className='flex flex-col gap-10 pr-10'>
         <ChromePicker color = {color} onChange={(e)=>setColor(e.hex)} />
-        <button type='button' className='p-2 rounded-md border border-black' onClick={clear}> Clear Canvas </button>
+        <button 
+          type='button' 
+          className='p-2 rounded-md border border-black' 
+          onClick={()=>socket.emit('clear')}
+        > 
+          Clear Canvas 
+        </button>
       </div>
       <canvas
         onMouseDown={onMouseDown}
---
+++ server/server.ts
@@ -0,0 +1,49 @@
+declare var require: any
+
+
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+
+
+import {Server} from 'socket.io'
+
+const io = new Server(server,{
+    cors: {
+        origin : '*'
+    }
+})
+
+type Point = {x:number , y:number}
+
+type DrawLine = {
+    prevPoint: Point | null,
+    currentPoint : Point,
+    color: string
+}
+
+io.on('connection', (socket)=>{
+    console.log('connection')
+
+    socket.on('client-ready',()=>{
+        socket.broadcast.emit('get-canvas-state')
+    })
+    
+    socket.on('canvas-state',(state)=>{
+        socket.broadcast.emit('canvas-state-from-server',state)
+    })
+
+    socket.on('draw-line',({prevPoint,currentPoint,color}: DrawLine)=>{
+        socket.broadcast.emit('draw-line',{prevPoint,currentPoint,color})
+    })
+
+    socket.on('clear',()=>{
+        io.emit('clear')
+    })
+})
+
+
+server.listen(5000,(err:string)=>{
+    err ? console.log(err) : console.log('server is running on port 5000')
+})
